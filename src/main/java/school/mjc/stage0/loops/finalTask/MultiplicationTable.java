99b53b20111caf1f22a90290c576d1c4de989ab7
Madinabonu <otkirbekovamadinabonu@gmail.com>
2022-08-26 11:09:09 +0300

    ....

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -1,7 +1,10 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
-    public void printTable(int numberTableToPrint){
+    public static void main(String[] args) {
+        printTable(5);
+    }
+    public static void printTable(int numberTableToPrint){
         for (int i = 1; i <= 10; i++) {
             System.out.println(i+" x "+numberTableToPrint+" = "+i*numberTableToPrint);
         }
